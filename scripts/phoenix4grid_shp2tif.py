1d332bdde1a7c5a9c0325f21a8101003f4f8fd45
Dhirendra Singh <dhi.singh@gmail.com>
2020-08-09 21:05:25 +1000

    Simplified conversion of phoenix shp to tif

--- scripts/phoenix4grid_shp2tif.py
+++ scripts/phoenix4grid_shp2tif.py
@@ -2,27 +2,17 @@
 import geostack
 from geostack.raster import Raster
 from geostack.vector import Vector
-from geostack.io import shapefileToVector
-from geostack.io import geoJsonToVector
-from geostack.io import vectorToShapefile
-from geostack.gs_enums import GeometryType
-from geostack.core import convert, ProjectionParameters
 from geostack.utils import get_epsg
-import numpy as np
 
 # TODO: Make this a script parameter
-pheonix4GridShapeFile = '../data/20181109_mountalex_evac_ffdi100d_grid.shp'
+shp = '../data/20181109_mountalex_evac_ffdi100d_grid.shp'
 
-print("Loading shapefile as vector " + pheonix4GridShapeFile)
-vector = shapefileToVector(pheonix4GridShapeFile)
-
-#print("Setting CRS to EPSG4326")
-#proj_EPSG4326 = ProjectionParameters.from_proj4("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-#vector.setProjectionParameters(proj_EPSG4326)
-
+print("Loading shapefile " + shp)
+vector = Vector.from_shapefile(shp)
+print("Setting CRS to EPSG:28355")
+vector = vector.convert(get_epsg(28355))
 print("Rasterising")
-raster = vector.rasterise(100.0, "output = HOUR_BURNT;")
-
-outfile = pheonix4GridShapeFile + ".tif"
-print("Writing " + outfile)
-raster.write(outfile);
+raster = vector.rasterise(10, "output = HOUR_BURNT;")
+outfile = shp + ".tif"
+print("Writing " + shp.replace("shp", "tif"))
+raster.write(outfile)
